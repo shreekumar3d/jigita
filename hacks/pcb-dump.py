875cbf10cf4a3a0cc1b9749d038d61600b4dcb3e
Shree Kumar <shree.shree@gmail.com>
2024-11-04 17:33:48 +0530

    Hacks: dump pad info in pcb dump

--- hacks/pcb-dump.py
+++ hacks/pcb-dump.py
@@ -2,6 +2,17 @@
 # etc
 import pcbnew
 import sys
+from pprint import pprint
+
+def dumpShape(shape):
+    result = []
+    for i in range(shape.OutlineCount()):
+        lc = shape.Outline(i)
+        for j in range(lc.PointCount()):
+            pt = lc.CPoint(j)
+            result.append([pcbnew.ToMM(pt.x),pcbnew.ToMM(pt.y)])
+    pprint(result)
+
 
 board = pcbnew.LoadBoard(sys.argv[1])
 fp_list = board.Footprints()
@@ -9,6 +20,31 @@ for fp in fp_list:
     print('Component ', fp.GetReference(), ' at ', fp.GetPosition())
     print('    Attribs = ', fp.GetAttributes()) # What are these ?
     print('    TH ? ', fp.HasThroughHolePads())
+    #pprint(dir(fp))
+    for pad in fp.Pads():
+        print('    Pad :', pad.GetPadName())
+        print('         position = ', pad.GetPosition())
+        print('         offset   = ', pad.GetPosition())
+        bb = pad.GetBoundingBox()
+        print('         bb       = ', bb.GetX(), bb.GetY(), ' w x h=, ',bb.GetWidth(), bb.GetHeight())
+        print('         radius   = ', pad.GetBoundingRadius())
+        print('         drill    = ', pad.GetDrillSize())
+        print('         drill shape   = ', pad.GetDrillShape())
+        shapes = [ 'CIRCLE', 'RECTANGLE', 'OVAL', 'TRAPEZOID', 'ROUNDRECT', 'CHAMFERED_RECT', 'CUSTOM']
+        print('         shape   = ', shapes[pad.GetShape()])
+        # pcbnew.PAD_DRILL_SHAPE_CIRCLE is 0
+        # GetShape() returns PAD_SHAPE defined in pcbnew/padstack.h
+        #   CIRCLE, (0)
+        #   RECTANGLE,
+        #   OVAL,
+        #   TRAPEZOID,
+        #   ROUNDRECT,
+        #   CHAMFERED_RECT
+        #   CUSTOM
+        # Haven't verified oval , but shape does have 4 points for rect
+        #   dumpShape(pad.GetEffectivePolygon())
+        #pprint(dir(pad))
+        #help(pad)
     for mod3d in fp.Models():
         print('    3D Model File = ', mod3d.m_Filename)
         print('        Rot = ', mod3d.m_Rotation)
